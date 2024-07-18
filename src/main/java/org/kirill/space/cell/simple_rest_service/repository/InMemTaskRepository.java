9de0972a2cfd7a5100eb3b8c8af58e62a60d3bab
Kirill Kozlov <space_cell@proton.me>
2024-07-18 11:43:28 +0500

    feat(repository): Added InMemTaskRepository implementation

---
+++ src/main/java/org/kirill/space/cell/simple_rest_service/repository/InMemTaskRepository.java
@@ -0,0 +1,35 @@
+package org.kirill.space.cell.simple_rest_service.repository;
+
+import org.kirill.space.cell.simple_rest_service.model.Task;
+import org.springframework.stereotype.Repository;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class InMemTaskRepository implements TaskRepository{
+
+    private final List<Task> tasks = new LinkedList<>() {{
+       this.add(new Task("First Task"));
+       this.add(new Task("Second Task"));
+    }};
+
+    @Override
+    public List<Task> findAll() {
+        return this.tasks;
+    }
+
+    @Override
+    public void save(Task task) {
+        this.tasks.add(task);
+    }
+
+    @Override
+    public Optional<Task> findById(UUID id) {
+        return this.tasks.stream()
+                .filter(task -> task.id().equals(id))
+                .findFirst();
+    }
+}
